93f9d94b828009897e9955d40f58aed83f934dd9
katien1234 <katie.n1234@gmail.com>
2019-02-08 10:33:35 +0000

    MockitoTesting

--- QuizAPI/target/QuizAPI/WEB-INF/classes/import.sql
+++ QuizAPI/target/QuizAPI/WEB-INF/classes/import.sql
@@ -6,7 +6,7 @@ INSERT INTO QUIZ (question, answer, category) VALUES ('As far as has ever been r
 INSERT INTO QUIZ (question, answer, category) VALUES ('Every day more money is printed for monopoly than the US Treasury', 'True','Fun');
 INSERT INTO QUIZ (question, answer, category) VALUES ('The fastest time to eat 15 Ferrero Rocher is 1 minute 10 seconds. ', 'False','Food');
 INSERT INTO QUIZ (question, answer, category) VALUES ('David Bowie declined a knighthood from the Queen', 'True','Music');
-INSERT INTO QUIZ (question, answer, category) VALUES ('Tom Cruise has never won an Oscar', 'True','katien123');
+INSERT INTO QUIZ (question, answer, category) VALUES ('Tom Cruise has never won an Oscar', 'True','Fame');
 INSERT INTO QUIZ (question, answer, category) VALUES ('Lightning never strikes in the same place twice.', 'False','Earth');
 INSERT INTO QUIZ (question, answer, category) VALUES ('Humans can’t breathe and swallow at the same time.', 'True','Health');
 INSERT INTO QUIZ (question, answer, category) VALUES ('There are more cells of bacteria in your body than there are human cells.', 'True','Health');
